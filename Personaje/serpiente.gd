8ea5eb0f0ecd9b8e34cef11692ccca6b7796c122
Carlos <olmedoreyescarlos580@gmail.com>
2025-04-09 18:11:34 +0200

    Diseño serpiente completado

--- Personaje/serpiente.gd
+++ Personaje/serpiente.gd
@@ -1,32 +1,44 @@
-extends CharacterBody2D
+extends CharacterBody2D 
 
-# Variables de movimiento
-@export var speed: float = 90.0
+@export var speed: float = 60.0
 @export var gravity: float = 900.0
+var moving_left = true
+var can_turn = true
 
-# Variables de animación
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var ground_ray: RayCast2D = $RayCast2D
 
 func _ready():
-	# Iniciar la animación de idle (reposo)
 	animated_sprite.play("Caminar")
 
-func _process(delta: float) -> void:
-	move_caracter()
+func _physics_process(delta: float) -> void:
+	move_character()
+	check_edge()
 
-func move_caracter():
-	# Aplicar gravedad
-	velocity.y += gravity  # Aplicar gravedad de forma independiente del frame
-	velocity.x = -speed  # Velocidad horizontal (puedes ajustarlo según lo que necesites)
-	
-	# Mover el nodo con el vector de velocidad
+func move_character():
+	velocity.y += gravity
+	velocity.x = -speed if moving_left else speed
 	move_and_slide()
 
-func _loselife():
-	# Código para perder vida
-	pass
+	# Voltear el sprite según la dirección
+	animated_sprite.flip_h = moving_left
 
+func check_edge():
+	if not ground_ray.is_colliding() and can_turn:
+		moving_left = !moving_left
+
+		# Reposicionar el RayCast al otro lado
+		ground_ray.position.x *= -1
+
+		can_turn = false
+		await get_tree().create_timer(0.2).timeout
+		can_turn = true
+
+func _loselife():
+	pass
+	
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):  # Verifica que el objeto que colisiona esté en el grupo "player"
+	if body.is_in_group("player"):
 		body._loselife()
 		pass
+
