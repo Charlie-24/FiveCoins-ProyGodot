6a392ffbe00d7f3e1385f6e7e289c319d912ba94
Carlos <olmedoreyescarlos580@gmail.com>
2025-04-10 08:21:04 +0200

    Menu pausa y PersonajePrincipal Actualizado

---
+++ UI/MenuPausa.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _input(event):  # Cambié 'func_input' por 'func _input', ya que esta es la función estándar para capturar eventos.
+	if event.is_action_pressed("pause"):  # 'is_action_pressed' está mal escrito como 'is_action_presed'.
+		print("Juego Pausado")
+		visible = not get_tree().paused
+		get_tree().paused = not get_tree().paused  # Esto es correcto para alternar la pausa.
